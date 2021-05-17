0cce0e11dbd759172bf9b7845fea3e679d23c74d
Erik <eriksostenessi07@gmail.com>
2021-05-17 10:57:55 -0500

    Se establecio la conexión con la base de datos

---
+++ database/database.go
@@ -0,0 +1,23 @@
+package database
+
+import (
+	"database/sql"
+	"log"
+	"os"
+)
+
+func ConexionDB() *sql.DB {
+	mysql := os.Getenv("mysql")
+	root := os.Getenv("root")
+	password := os.Getenv("Loindeseable09")
+	table := os.Getenv("store")
+
+	db, err := sql.Open(mysql, root+":"+password+"@tcp(127.0.0.1:3306)/"+table)
+
+	if err != nil {
+		log.Panic("Ocurrio un problema al conectarse a la base de datos")
+	}
+	log.Print("Conexión establecida")
+
+	return db
+}
